5304fe39d9b99679444ad72a81b309829e763b21
Justin Beaulieu <beaulieudev@gmail.com>
2018-11-23 22:08:53 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize
+    @word = word
+  end
+
+  def match(string1, string2)
+   array1 = string1.downcase.gsub(/[\s+-]/,'').split("").sort
+   array2 = string2.downcase.gsub(/[\s+-]/,'').split("").sort
+   return array1 == array2
+ end
+end
